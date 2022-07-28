0f7fba28714a1ca270614a7082eb50f0745b0692
Jonas Lima de Amorim <jonas1307@live.com>
2022-07-28 16:14:08 -0300

    chore: confguring swagger versioning

--- src/DistributedCache.WebApi/Program.cs
+++ src/DistributedCache.WebApi/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +7,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(s =>
+{
+    s.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "DistributedCache",
+        Version = "1.0"
+    });
+});
 
 var app = builder.Build();
 
