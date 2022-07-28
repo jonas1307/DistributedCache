c4a3fdee62f8317a6722f2cecc20bf47f81391fe
Jonas Lima de Amorim <jonas1307@live.com>
2022-07-28 17:16:31 -0300

    feat: mocking CacheController

---
+++ src/DistributedCache.WebApi/Controllers/V1/CacheController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DistributedCache.WebApi.Controllers.V1
+{
+    [ApiController]
+    [ApiExplorerSettings(GroupName = "v1")]
+    [Route("v1/[controller]")]
+    public class CacheController : DefaultControllerBase
+    {
+        private readonly ILogger<CacheController> _logger;
+
+        public CacheController(ILogger<CacheController> logger) : base(logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index() => await AsyncResult(async () =>
+        {
+            _logger.LogInformation($"({nameof(CacheController)}) New request received");
+
+            return Ok();
+        });
+    }
+}
