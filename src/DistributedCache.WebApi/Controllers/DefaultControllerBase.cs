1cb23cc7c59d31beba2a37ff1c0a696181b84c78
Jonas Lima de Amorim <jonas1307@live.com>
2022-07-28 16:56:37 -0300

    feat: implementing base controller

---
+++ src/DistributedCache.WebApi/Controllers/DefaultControllerBase.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DistributedCache.WebApi.Controllers
+{
+    public class DefaultControllerBase : ControllerBase
+    {
+        private readonly ILogger _logger;
+
+        public DefaultControllerBase(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        protected async Task<IActionResult> AsyncResult(Func<Task<IActionResult>> func)
+        {
+            try
+            {
+                return await func();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred while processing the request. Try again later.");
+
+                return StatusCode(500, new { message = "An error occurred while processing the request. Try again later." });
+            }
+        }
+    }
+}
