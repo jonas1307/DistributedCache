fc14d27973d6a603ddae8f2f3bedcd105e1d8a68
Jonas Lima de Amorim <jonas1307@live.com>
2022-07-28 18:04:51 -0300

    chore: adding serilog

--- src/DistributedCache.WebApi/Program.cs
+++ src/DistributedCache.WebApi/Program.cs
@@ -1,8 +1,11 @@
+using DistributedCache.WebApi.Utils;
 using Microsoft.OpenApi.Models;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Host.AddSerilog();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
---
+++ src/DistributedCache.WebApi/Utils/SerilogExtension.cs
@@ -0,0 +1,17 @@
+﻿using Serilog;
+using Serilog.Events;
+
+namespace DistributedCache.WebApi.Utils
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddSerilog(this IHostBuilder host)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Async(wt => wt.Console(LogEventLevel.Verbose, "[{Level:u3} {Timestamp:yyyy-MM-dd HH:mm:ss}] {Message:lj}{NewLine}{Exception}"))
+                .CreateLogger();
+
+            host.UseSerilog(Log.Logger);
+        }
+    }
+}
