333970b108d08d046f199e301b5ede02111d223d
Jonas Lima de Amorim <jonas1307@live.com>
2022-07-28 18:56:59 -0300

    feat: implementing LocalCache

---
+++ src/DistributedCache.Caching/Contracts/ILocalCache.cs
@@ -0,0 +1,9 @@
+﻿namespace DistributedCache.Caching.Contracts
+{
+    public interface ILocalCache
+    {
+        T? Get<T>(string id);
+        bool Set<T>(T value, string id, TimeSpan? expiration = null);
+        bool Remove(string id);
+    }
+}
---
+++ src/DistributedCache.Caching/LocalCache.cs
@@ -0,0 +1,41 @@
+﻿using DistributedCache.Caching.Contracts;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace DistributedCache.Caching
+{
+    public class LocalCache : ILocalCache
+    {
+        private readonly IMemoryCache _cache;
+        private readonly TimeSpan DefaultExpiration = TimeSpan.FromMinutes(5);
+
+        public LocalCache(IMemoryCache memoryCache)
+        {
+            _cache = memoryCache;
+        }
+
+        public T? Get<T>(string id)
+        {
+            if (string.IsNullOrEmpty(id)) return default;
+
+            if (_cache.TryGetValue(id, out T value)) return value;
+
+            return default;
+        }
+
+        public bool Set<T>(T value, string id, TimeSpan? expiration = null)
+        {
+            if (value is null) return false;
+
+            _cache.Set<T>(id, value, expiration ?? DefaultExpiration);
+
+            return true;
+        }
+
+        public bool Remove(string id)
+        {
+            _cache.Remove(id);
+
+            return true;
+        }
+    }
+}
